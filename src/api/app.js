1038bac2e3f49e3d6e07cf37bfa3d3109e53ec07
LenzChristofer <lenzchristo@gmail.com>
2022-02-20 19:27:51 -0300

    Created userRouter and MongoDB connection

--- src/api/app.js
+++ src/api/app.js
@@ -1,11 +1,48 @@
+/* eslint-disable sonarjs/cognitive-complexity */
 const express = require('express');
 
+const expressAsyncHandler = require('express-async-handler');
+
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+
+const User = require('../models/userModel');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+});
+
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
   response.send();
 });
 // Não remover esse end-point, ele é necessário para o avaliador
-
+app.post('/users', expressAsyncHandler(async (req, res) => {
+  const usedEmail = await User.findOne({ email: req.body.email });
+  if (!usedEmail) {
+      const user = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          role: 'user',
+      });
+      const createUser = user.save();
+      
+      if (createUser) {
+          res.send(user);
+      } else {
+          res.status(400).send({ message: 'Invalid entries. Try again' });
+      }
+  } else {
+    res.status(409).send({ message: 'Email already registered' });
+  }
+  }));
 module.exports = app;
